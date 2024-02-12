7d9f2dd67bc3b5828d1756d8beb7d0582fb3fe1b
Olesya <118808502+olastt@users.noreply.github.com>
2024-02-12 22:54:00 +0300

    Update home_2.py

--- home_2.py
+++ home_2.py
@@ -1,3 +1,22 @@
 # Дан список my_list = ['a', 'b', [1, 2, 3], 'd']. Распечатайте значения 1, 2, 3
 my_list = ['a', 'b', [1, 2, 3], 'd']
 print(*my_list[2])
+
+
+# Дан список list_1 = ['Hi', 'ananas', 2, 75, 'pizza', 36, 100]
+#    - получите сумму всех чисел,
+#    - распечатайте все строки, где есть буква 'a'
+list_1 = ['Hi', 'ananas', 2, 75, 'pizza', 36, 100]
+sum_of_numbers = 0
+
+for i in list_1:
+    if isinstance(i, int):
+        sum_of_numbers += i
+
+print("Сумма всех чисел в списке:", sum_of_numbers)
+
+list_1 = ['Hi', 'ananas', 2, 75, 'pizza', 36, 100]
+
+for i in list_1:
+    if isinstance(i, str) and 'a' in i:
+        print(i)
