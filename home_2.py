91aaefd3d84ec82834096bf48be29d86aa556c11
Olesya <118808502+olastt@users.noreply.github.com>
2024-02-12 23:12:01 +0300

    Update home_2.py

--- home_2.py
+++ home_2.py
@@ -20,3 +20,22 @@ list_1 = ['Hi', 'ananas', 2, 75, 'pizza', 36, 100]
 for i in list_1:
     if isinstance(i, str) and 'a' in i:
         print(i)
+
+# Превратите лист ['cat', 'dog', 'horse', 'cow'] в кортеж
+a = ['cat', 'dog', 'horse', 'cow']
+print(tuple(a))
+
+#  Напишите программу, которая определяет, какая семья больше. 
+#      1) Программа имеет два input() - например, family_1, family_2. 
+#      2) Членов семьи нужно перечислить через запятую. 
+#     Ожидаемый результат - программа выводит семью с бОльшим составом. Если состав одинаковый, print("Equal')
+family_1 = input().split(",")
+family_2 = input().split(",")
+print(family_1)
+print(family_2)
+if len(family_1) > len(family_2):
+    print('Семья 1 больше')
+elif len(family_2) > len(family_1):
+    print('Семья 2 больше')
+else:
+    print('Equal')
